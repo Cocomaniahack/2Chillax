845ba6c4fde06baac630bd4c4ba9f48a9c1a0b00
coco <coco@MacBook-Air-de-coco.local>
2019-12-13 13:26:54 +0100

    1 commit chillax

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express(); // here I use the express() method, instead of the createServer()
+
+//app.get('/', function(req, res){
+  //res.send('Hello World');
+//});
+
+//app.set('view engine', 'html');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile('view/index.html' , { root : __dirname});
+})
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
